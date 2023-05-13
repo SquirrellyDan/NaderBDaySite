777e662618e3980b3cbd3025d8da0d2ee964c500
eeinoder <nicolaeeinoder@gmail.com>
2023-05-12 21:18:50 -0700

    Added touchstart eventlistener for mobile

--- index.js
+++ index.js
@@ -3,14 +3,17 @@
 //import JSConfetti from 'js-confetti'
 // don't need above, got js-confetti from CDN download -> script tag in HTML
 
-const jsConfetti = new JSConfetti()
+const jsConfetti = new JSConfetti();
 
 function confettify() {
-  jsConfetti.addConfetti()
+  jsConfetti.addConfetti();
 }
 
 // Confetti on page load
-confettify()
+confettify();
 
 // Confetti on click body (anywhere)
-document.defaultView.addEventListener("click", confettify)
+document.defaultView.addEventListener("click", confettify);
+
+// or on touch start (on mobile, user taps and trigger on contact)
+document.defaultView.addEventListener("touchstart", confettify);
